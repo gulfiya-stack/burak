0691e69335eda50209d7a75ea870791e168cf268
Gulfiya <atasheva.gul1995@gmail.com>
2026-09-20 21:11:03 +0900

    feat: task-l

---
+++ src/train.ts
@@ -0,0 +1,19 @@
+//TASK-L
+
+function reverseSentence(text: string) {
+    let words = text.split(" ");
+    let result = "";
+
+    for (let i of words) {
+        let reversed = i
+            .split("")
+            .reverse()
+            .join("") + " ";
+
+        result = result + reversed;
+    }
+
+    console.log(result);
+}
+
+reverseSentence("we like coding!");
