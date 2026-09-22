be97f2a5bec7a4a3d1c7d3cdec4f079ca7944c51
Gulfiya <atasheva.gul1995@gmail.com>
2026-09-22 15:36:35 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+
+/**1-ENTRANCE**/
+const app = express();
+console.log("dirname: ", __dirname);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/**2-SESSIONS**/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+/**3-VIEWS**/
+/**4-ROUTERS**/
+
+export default app; //module.exports = app; 
+
+
+
