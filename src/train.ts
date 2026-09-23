ed9e126644b7c59571e2abdc69c713488dfaa53d
Gulfiya <atasheva.gul1995@gmail.com>
2026-09-23 14:56:17 +0900

    feat: task-m

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,32 @@
-//TASK-L
-function reverseSentence(text: string) {
-    let words = text.split(" ");
-    let result = "";
+//TASK M
 
-    for (let i of words) {
-        let reversed = i
-            .split("")
-            .reverse()
-            .join("") + " ";
+// Array ichidagi har bir raqam uchun raqamning o'zi va uning kvadratidan tashkil topgan object hosil qilib qaytarsin.
 
-        result = result + reversed;
+// Masalan: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, ...]
+
+function getSquareNumbers(list: number[]) {
+    for (let i = 0; i < list.length; i++) {
+        console.log(`number: ${list[i]}, square: ${list[i] * list[i]} `);
     }
 
-    console.log(result);
 }
+getSquareNumbers([1, 2, 3]);
 
-reverseSentence("we like coding!");
+//TASK-L
+// function reverseSentence(text: string) {
+//     let words = text.split(" ");
+//     let result = "";
+
+//     for (let i of words) {
+//         let reversed = i
+//             .split("")
+//             .reverse()
+//             .join("") + " ";
+
+//         result = result + reversed;
+//     }
+
+//     console.log(result);
+// }
+
+// reverseSentence("we like coding!");
