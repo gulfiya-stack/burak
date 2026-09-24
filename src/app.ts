782980e3be0efb0000f0a6cfd380f3e52c58ba92
Gulfiya <atasheva.gul1995@gmail.com>
2026-09-25 01:10:43 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /**1-ENTRANCE**/
 const app = express();
@@ -14,8 +15,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 /**3-VIEWS**/
 /**4-ROUTERS**/
-app.use("/", router); // (Design) Middleware pattern
-
+app.use("/admin", routerAdmin); //EJS, BSSR
+app.use("/", router); // React // (Design) Middleware pattern
+// Goal1: SPA: REACT (REST API)
+// Goal2: BSSR: EJS
 
 export default app; //module.exports = app; 
 
--- src/router.ts
+++ src/router.ts
@@ -3,11 +3,20 @@ const router = express.Router();
 import memberController from "./controllers/member.controller";
 
 
-router.get("/", memberController.goHome);
-
-router.get("/login", memberController.getLogin);
-
-router.get("/signup", memberController.getSignup);
 
 
 export default router;
+
+
+
+
+
+
+
+
+
+// router.get("/", memberController.goHome);
+
+// router.get("/login", memberController.getLogin);
+
+// router.get("/signup", memberController.getSignup);
